03ca4d6639a98ae8274e841ac709fde6fc430598
eduardomaldonadofernandez21 <55493415+eduardomaldonadofernandez21@users.noreply.github.com>
2020-01-23 11:23:02 +0000

    Add files via upload

---
+++ kata2/Kata2.java
@@ -0,0 +1,19 @@
+package kata2;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Kata2 {
+
+    public static void main(String[] args) {
+        String [] data = {"Rosa", "Pepe", "María", "Pepe", "Pepe", "Rosa"};
+        Histogram histo = new Histogram(data);
+        Map<String,Integer> histogr = histo.getHistogram();
+        
+        for (String key : histogr.keySet()) {
+            System.out.println(key + " ==> " + histogr.get(key));
+        }
+      
+    }
+    
+}
